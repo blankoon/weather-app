0cb525a0b990a93c8aa654fb34af84dad02bb986
Isu Ahn <clat.dev@gmail.com>
2019-04-14 21:35:53 +0900

    create loading weather screen

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  }
+
   render() {
+    const { isLoaded } = this.state
+    
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the weather</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -13,11 +23,21 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, // 이 설정을 하지 않으면 전체화면이 되지 않음
     backgroundColor: '#fff',
     alignItems: 'center', // 세로정렬: flex-start, flex-end, space-between, space-around, stretch
     justifyContent: 'center', // 가로정렬
     // flexDirection: 'row' // Default: column
     // flexWrap: 'wrap' // nowrap
   },
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100,
+  }
 });
